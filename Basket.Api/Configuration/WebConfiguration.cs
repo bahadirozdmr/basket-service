0217ba4c24d61ee069822f8b53e130f224ad0ebb
bahadirozdemir <bahadirozdmr@gmail.com>
2022-03-02 21:59:00 +0300

    prometheus config;

--- Basket.Api/Configuration/WebConfiguration.cs
+++ Basket.Api/Configuration/WebConfiguration.cs
@@ -4,6 +4,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Prometheus;
 
 namespace Basket.Api.Configuration
 {
@@ -30,9 +31,11 @@ namespace Basket.Api.Configuration
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseHttpMetrics();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapMetrics();
             });
 
 
